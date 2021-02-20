ad30c4709c94c4274ae2562af0ffc65ea6a081f2
Bios Sun <biossun@gmail.com>
2021-02-20 18:16:45 +0800

    在 postpublish 阶段时还原 packge.json，并清理生成的 publish 目录

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -1,15 +1,18 @@
-const fs = require('fs-extra')
 const path = require('path')
+const fs = require('fs-extra')
+const del = require('del')
 const { eachPackages, buildPackages } = require('./utils')
 
 eachPackages((package) => {
+    // 还原 package.json 中的 main 及 module 配置
     const filePath = path.join(package.path, 'package.json')
 
     const publishingInfo = { ...package.info }
-    publishingInfo.main = 'publish/index.js'
-    publishingInfo.module = 'publish/index.js'
+    publishingInfo.main = 'lib/index.ts'
+    publishingInfo.module = 'lib/index.ts'
 
     fs.writeJSONSync(filePath, publishingInfo, { spaces: 4 })
-})
 
-buildPackages()
+    // 删除 publish 目录
+    del([path.join(package.path, 'publish')])
+})
