d6c5c74925da7ceda15e3ae6469f7e0497208e7e
Bios Sun <biossun@gmail.com>
2021-02-25 20:57:40 +0800

    website - 修复首页内容在页面加载时布局跳动的问题

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 import Layout from '@theme/Layout'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import useBaseUrl from '@docusaurus/useBaseUrl'
@@ -8,21 +8,16 @@ function Home() {
     const context = useDocusaurusContext()
     const { siteConfig = {} } = context
 
-    useEffect(() => {
-        document.documentElement.setAttribute('data-index', 'true');
+    useLayoutEffect(() => {
+        document.documentElement.setAttribute('data-index', 'true')
 
         return () => {
-            document.documentElement.removeAttribute('data-index');
+            document.documentElement.removeAttribute('data-index')
         }
-    });
+    })
 
     return (
-        <Layout
-            className="without"
-            description="一套基于 React 实现的组件库，混乱中立。"
-            // image=""
-            // keywords={[]}
-        >
+        <Layout description="一套基于 React 实现的组件库，混乱中立。">
             <main className={styles.main}>
                 <img
                     className={styles.logo}
